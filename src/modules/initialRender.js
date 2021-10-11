f74db5e6c74537159072369c206a9cc0e55bbd47
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-10-11 23:33:19 +0100

    Added modules folder

---
+++ src/modules/initialRender.js
@@ -0,0 +1,9 @@
+// TODO: render header section
+
+// TODO: render navigation section
+
+// TODO: render Todo section
+
+// TODO: add existing projects from localStorage to navigation section
+
+// TODO: add Todos from localStorage (if any) to Todo section

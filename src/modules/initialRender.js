39a805a17e4f3892b7c198b2aa5785179b10fed1
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-10-13 12:37:32 +0100

    Added new features folder and renamed initialRender to renderUI

--- src/modules/initialRender.js
+++
@@ -1,9 +0,0 @@
-// TODO: render header section
-
-// TODO: render navigation section
-
-// TODO: render Todo section
-
-// TODO: add existing projects from localStorage to navigation section
-
-// TODO: add Todos from localStorage (if any) to Todo section
---
+++ src/modules/renderUI.js
@@ -0,0 +1,19 @@
+// TODO: import UI elements
+import renderHeader from './header';
+
+// TODO: render UI
+
+function renderUI() {
+  const content = document.getElementById('content');
+  // TODO: render header section
+  content.appendChild(renderHeader());
+  // TODO: render navigation section
+
+  // TODO: render Todo section
+
+  // TODO: add existing projects from localStorage to navigation section
+
+  // TODO: add Todos from localStorage (if any) to Todo section
+}
+
+export default renderUI;

232a5415e7b74f1d679aafd9c78fd2665cd3aaf0
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-10-14 22:59:29 +0100

    Added project title h2 element

--- src/modules/nav.js
+++ src/modules/nav.js
@@ -3,6 +3,10 @@ const renderNav = () => {
   const links = document.createElement('ul');
   links.classList.add('links');
   nav.appendChild(links);
+  const projectsTitle = document.createElement('h2');
+  projectsTitle.classList.add('projects-title');
+  projectsTitle.textContent = 'Projects';
+  links.appendChild(projectsTitle);
 
   return nav;
 };

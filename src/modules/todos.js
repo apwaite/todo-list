0602a61ba1bf3cc7df0ea922e0e0821615a6ea70
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-10-16 21:38:15 +0100

    Refactored element name and className

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,6 +1,16 @@
+// const projects = [];
+
+// const newProject = (title, description, dueDate, priority, notes) => ({
+//   title,
+//   description,
+//   dueDate,
+//   priority,
+//   notes,
+// });
+
 const renderTodos = () => {
-  const todos = document.createElement('main');
-  todos.classList.add('todos');
+  const todos = document.createElement('section');
+  todos.classList.add('main');
   return todos;
 };
 

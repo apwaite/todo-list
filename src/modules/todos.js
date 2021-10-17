8d77fee0a2b64d8c982ec25e59a48317c5f3e239
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-10-17 21:24:51 +0100

    Added TODO

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -21,6 +21,7 @@ const defaultProject = newProject(
 const renderTodos = () => {
   const todos = document.createElement('section');
   todos.classList.add('main');
+  // TODO: add some basic elements and display placeholder data
   return todos;
 };
 

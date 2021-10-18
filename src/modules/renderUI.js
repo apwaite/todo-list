b85115e761ba0724e2518b53872b78deee1ba5ec
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-10-18 23:52:45 +0100

    Added main section to renderUI and loadedDefault todo

--- src/modules/renderUI.js
+++ src/modules/renderUI.js
@@ -1,8 +1,10 @@
 // TODO: import UI elements
 import renderHeader from './header';
+import renderMain from './main';
 import renderNav from './nav';
 import renderTodos from './todos';
-import { projects, newProject, loadDefault } from './projects';
+import { loadDefault } from './projects';
+import renderFooter from './footer';
 
 // TODO: render UI
 
@@ -10,13 +12,16 @@ function renderUI() {
   const content = document.getElementById('content');
   // TODO: render header section
   content.appendChild(renderHeader());
+  content.appendChild(renderMain());
+  const main = document.querySelector('.main');
   // TODO: render navigation section
-  content.appendChild(renderNav());
+  main.appendChild(renderNav());
   // TODO: render Todo section
-  content.appendChild(renderTodos());
+  main.appendChild(renderTodos());
   // TODO: add existing projects from localStorage to navigation section
   loadDefault();
   // TODO: add Todos from localStorage (if any) to Todo section
+  // TODO: add footer to page
 }
 
 export default renderUI;

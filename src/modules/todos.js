26e10791ccde27674a91c8fc8658406c62d778b5
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-10-18 23:49:32 +0100

    Moved the creation of main section to its own file for Single Responsibility purposes and to allow todos to be appended independently by renderUI

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,13 +1,10 @@
 console.log('todos connected!');
 
 const renderTodos = () => {
-  const main = document.createElement('section');
-  main.classList.add('main');
   // TODO: add some basic elements and display placeholder data
   const todos = document.createElement('div');
   todos.classList.add('todos');
-  main.appendChild(todos);
-  return main;
+  return todos;
 };
 
 export default renderTodos;

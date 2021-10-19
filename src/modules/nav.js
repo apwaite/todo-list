8a8b712c9e6da414787f1e4a7cca522d79724de2
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-10-19 23:47:12 +0100

    Made better use of whitespace to increase readability

--- src/modules/nav.js
+++ src/modules/nav.js
@@ -6,21 +6,26 @@
 
 const renderNav = () => {
   const nav = document.createElement('nav');
+
   const links = document.createElement('ul');
   links.classList.add('links');
   nav.appendChild(links);
+
   const projectsTitle = document.createElement('h2');
   projectsTitle.classList.add('projects-title');
   projectsTitle.textContent = 'Projects';
   links.appendChild(projectsTitle);
+
   const existingProjects = document.createElement('li');
   existingProjects.classList.add('existing-projects');
   existingProjects.textContent = 'Existing Project Placeholder';
   links.appendChild(existingProjects);
+
   const addNewProject = document.createElement('li');
   addNewProject.classList.add('add-new-project');
   addNewProject.textContent = 'Add New Project';
   links.appendChild(addNewProject);
+
   return nav;
 };
 

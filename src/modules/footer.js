50e42d657acfbc9abce253519bac21ece6112702
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-10-19 23:46:28 +0100

    Made better use of whitespace to increase readability

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -1,13 +1,16 @@
 const renderFooter = () => {
   const footer = document.createElement('footer');
+
   const madeBy = document.createElement('p');
   madeBy.classList.add('made-by');
   madeBy.textContent = 'Made with ❤️ by Andrew';
   footer.appendChild(madeBy);
+
   const link = document.createElement('a');
   link.href = 'https://github.com/apwaite';
   link.target = '_blank';
   madeBy.appendChild(link);
+
   const icon = document.createElement('i');
   icon.classList.add('fab', 'fa-github');
   link.appendChild(icon);
--- src/modules/header.js
+++ src/modules/header.js
@@ -1,9 +1,11 @@
 const renderHeader = () => {
   const header = document.createElement('header');
+
   const h1 = document.createElement('h1');
   h1.classList.add('header-title');
   h1.textContent = 'Todo List';
   header.appendChild(h1);
+
   return header;
 };
 

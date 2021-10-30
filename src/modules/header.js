9a8a7520071a952400c38d9facbaefe826476adb
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-10-30 15:44:29 +0100

    Added font awesome icon to h1

--- src/modules/header.js
+++ src/modules/header.js
@@ -1,9 +1,13 @@
 const renderHeader = () => {
   const header = document.createElement('header');
 
+  const icon = document.createElement('h1');
+  icon.innerHTML = '<i class="fas fa-clipboard-list"></i>';
+  header.appendChild(icon);
+
   const h1 = document.createElement('h1');
   h1.classList.add('header-title');
-  h1.textContent = 'Todo List';
+  h1.innerText = 'Todo List';
   header.appendChild(h1);
 
   return header;

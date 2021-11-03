1b84d539725f355ba201ed6ada356b9c17cae2ee
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-11-03 23:55:26 +0000

    Added project name

--- src/modules/new-project-form.js
+++ src/modules/new-project-form.js
@@ -2,6 +2,11 @@ const newProjectForm = () => {
   const form = document.createElement('div');
   form.classList.add('form');
   // TODO: add new project form
+  const projectName = document.createElement('h2');
+  projectName.classList.add('project-name');
+  projectName.textContent = 'Project Title:';
+  form.appendChild(projectName);
+
   return form;
 };
 

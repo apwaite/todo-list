9536c5b75f2bac49055c664aa95f22fa1820f66c
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-11-04 23:57:56 +0000

    Changed from h2 to h3

--- src/modules/new-project-form.js
+++ src/modules/new-project-form.js
@@ -2,7 +2,7 @@ const newProjectForm = () => {
   const form = document.createElement('div');
   form.classList.add('form');
   // TODO: add new project form
-  const projectName = document.createElement('h2');
+  const projectName = document.createElement('h3');
   projectName.classList.add('project-name');
   projectName.textContent = 'Project Title:';
   form.appendChild(projectName);

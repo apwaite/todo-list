5d7030eb61d37dfc8dd11f354ec5a8b3e568944a
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2021-11-25 23:18:32 +0000

    Added additional classes for improved styling

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -3,13 +3,22 @@ const renderFooter = () => {
 
   const madeBy = document.createElement('p');
   madeBy.classList.add('made-by');
-  madeBy.textContent = 'Made with ❤️ by Andrew';
+  madeBy.innerText = 'Made with ';
+  const emoji = document.createElement('p');
+  emoji.classList.add('emoji');
+  emoji.innerText = '❤️ ';
+  const name = document.createElement('p');
+  name.classList.add('name');
+  name.innerText = 'by Andrew';
+
   footer.appendChild(madeBy);
+  footer.appendChild(emoji);
+  footer.appendChild(name);
 
   const link = document.createElement('a');
   link.href = 'https://github.com/apwaite';
   link.target = '_blank';
-  madeBy.appendChild(link);
+  name.appendChild(link);
 
   const icon = document.createElement('i');
   icon.classList.add('fab', 'fa-github');

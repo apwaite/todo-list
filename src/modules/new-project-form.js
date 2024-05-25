e33129b3dbd5f8b35d5f25215af4696d54e6a10c
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2024-05-25 21:07:59 +0100

    Added close element to project form. Removed TODO.

--- src/modules/new-project-form.js
+++ src/modules/new-project-form.js
@@ -10,15 +10,17 @@
 
 const renderProjectForm = () => {
   const form = document.createElement('div');
-
   form.classList.add('form', 'form-styling');
-
-  // TODO: Add close form button
 
   const projectTitle = document.createElement('h3');
   projectTitle.classList.add('project-title');
   projectTitle.textContent = 'Project Title:';
   form.appendChild(projectTitle);
+
+  const projectClose = document.createElement('span');
+  projectClose.classList.add('close-form');
+  projectClose.textContent = 'x';
+  form.appendChild(projectClose);
 
   const projectInput = document.createElement('input');
   projectInput.classList.add('project-input');

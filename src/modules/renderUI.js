25d23828aa515f2e7e5a6e3a3636a99814a4a8a2
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2024-05-26 22:46:43 +0100

    Added reworked form imports. Restructured form event listeners. Cleaned up existing code

--- src/modules/renderUI.js
+++ src/modules/renderUI.js
@@ -1,27 +1,33 @@
 // TODO: import UI elements
 import renderHeader from './header';
 import renderMain from './main';
-import { createNavEvents, renderNav } from './nav';
+import { addProjectBtn, hamburgerMenu, renderNav } from './nav';
 import renderTodos from './todos';
 import { loadDefault, projects } from './projects';
 import renderFooter from './footer';
 import { closeNewProjectForm, renderProjectForm } from './newProjectForm';
 
 function renderUI() {
+  // Append rendered elements to DOM
   const content = document.getElementById('content');
-  const form = document.getElementById('form-overlay');
   content.appendChild(renderHeader());
   content.appendChild(renderMain());
+  content.appendChild(renderFooter());
+
   const main = document.querySelector('.main');
   main.appendChild(renderNav());
   main.appendChild(renderTodos());
-  // TODO: add existing projects from localStorage to navigation section
-  loadDefault();
-  // TODO: add Todos from localStorage (if any) to Todo section
-  content.appendChild(renderFooter());
+
+  const form = document.getElementById('form-overlay');
   form.appendChild(renderProjectForm());
-  createNavEvents();
+
+  // Add event listeners
+  addProjectBtn();
+  hamburgerMenu();
   closeNewProjectForm();
+
+  // Load existing projects from localStorage
+  loadDefault();
 }
 
 export default renderUI;

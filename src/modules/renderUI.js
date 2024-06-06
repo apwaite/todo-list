f74182939d74cec5ae9038b2719e80873b47d1c3
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2024-06-06 02:16:57 +0100

    Imported clear button and added event listener

--- src/modules/renderUI.js
+++ src/modules/renderUI.js
@@ -7,6 +7,7 @@ import { projects, loadDefault } from './projects';
 import renderFooter from './footer';
 import {
   closeProjectForm,
+  clearBtn,
   addProjectBtn,
   renderProjectForm,
 } from './newProjectForm';
@@ -28,6 +29,7 @@ function renderUI() {
   // Add event listeners
   addNewProjectBtn();
   hamburgerMenu();
+  clearBtn();
   addProjectBtn();
   closeProjectForm();
 

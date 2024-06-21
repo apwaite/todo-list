575c8d81b242679c404c3dc3eaef40fd90ea592f
Andrew Waite <18730347+apwaite@users.noreply.github.com>
2024-06-21 21:22:12 +0100

    Added new todo button

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,9 +1,23 @@
+// import { projects } from './projects';
+
 console.log('todos connected!');
+
+// const projectTodos = [];
+
+// const defaultTodos = [];
+
+// const addTodoBtn = () => {
+
+// }
 
 const renderTodos = () => {
   const todos = document.createElement('div');
   todos.classList.add('todos');
 
+  const addNewTodo = document.createElement('button');
+  addNewTodo.classList.add('new-todo-btn');
+  addNewTodo.textContent = '+';
+  todos.appendChild(addNewTodo);
   return todos;
 };
 
